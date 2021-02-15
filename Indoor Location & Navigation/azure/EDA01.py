71ec328fc02308cda7e354bb22fe55354c78b88e
gnbrganchan <53957532+gnbrganchan@users.noreply.github.com>
2021-02-15 10:28:43 +0900

    Create EDA01.py

---
+++ Indoor Location & Navigation/azure/EDA01.py
@@ -0,0 +1,344 @@
+from azureml.core import Workspace,Datastore
+
+subscription_id = ''
+resource_group  = ''
+workspace_name  = ''
+
+try:
+    ws = Workspace(subscription_id = subscription_id, resource_group = resource_group, workspace_name = workspace_name)
+    ws.write_config()
+    print('Library configuration succeeded')
+except:@dataclass
+class ReadData:
+    acce: np.ndarray
+    acce_uncali: np.ndarray
+    gyro: np.ndarray
+    gyro_uncali: np.ndarray
+    magn: np.ndarray
+    magn_uncali: np.ndarray
+    ahrs: np.ndarray
+    wifi: np.ndarray
+    ibeacon: np.ndarray
+    waypoint: np.ndarray
+
+
+def read_data_file(data_filename):
+    acce = []
+    acce_uncali = []
+    gyro = []
+    gyro_uncali = []
+    magn = []
+    magn_uncali = []
+    ahrs = []
+    wifi = []
+    ibeacon = []
+    waypoint = []
+
+    with open(data_filename, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+    for line_data in lines:
+        line_data = line_data.strip()
+        if not line_data or line_data[0] == '#':
+            continue
+
+        line_data = line_data.split('\t')
+
+        if line_data[1] == 'TYPE_WAYPOINT':
+            waypoint.append([int(line_data[0]), float(line_data[2]), float(line_data[3])])
+            continue
+       
+        if line_data[1] == 'TYPE_ACCELEROMETER':
+            acce.append([int(line_data[0]), float(line_data[2]), float(line_data[3]), float(line_data[4])])
+            continue
+        
+        if line_data[1] == 'TYPE_ACCELEROMETER_UNCALIBRATED':
+            acce_uncali.append([int(line_data[0]), float(line_data[2]), float(line_data[3]), float(line_data[4])])
+            continue
+        
+        if line_data[1] == 'TYPE_GYROSCOPE':
+            gyro.append([int(line_data[0]), float(line_data[2]), float(line_data[3]), float(line_data[4])])
+            continue
+
+        if line_data[1] == 'TYPE_GYROSCOPE_UNCALIBRATED':
+            gyro_uncali.append([int(line_data[0]), float(line_data[2]), float(line_data[3]), float(line_data[4])])
+            continue
+        
+        if line_data[1] == 'TYPE_MAGNETIC_FIELD':
+            magn.append([int(line_data[0]), float(line_data[2]), float(line_data[3]), float(line_data[4])])
+            continue
+
+        if line_data[1] == 'TYPE_MAGNETIC_FIELD_UNCALIBRATED':
+            magn_uncali.append([int(line_data[0]), float(line_data[2]), float(line_data[3]), float(line_data[4])])
+            continue
+
+        if line_data[1] == 'TYPE_ROTATION_VECTOR':
+            ahrs.append([int(line_data[0]), float(line_data[2]), float(line_data[3]), float(line_data[4])])
+            continue
+
+        if line_data[1] == 'TYPE_WIFI':
+            sys_ts = line_data[0]
+            ssid = line_data[2]
+            bssid = line_data[3]
+            rssi = line_data[4]
+            lastseen_ts = line_data[6]
+            wifi_data = [sys_ts, ssid, bssid, rssi, lastseen_ts]
+            wifi.append(wifi_data)
+            continue
+
+        if line_data[1] == 'TYPE_BEACON':
+            ts = line_data[0]
+            uuid = line_data[2]
+            major = line_data[3]
+            minor = line_data[4]
+            rssi = line_data[6]
+            ibeacon_data = [ts, '_'.join([uuid, major, minor]), rssi]
+            ibeacon.append(ibeacon_data)
+            continue
+        
+    
+    acce = np.array(acce)
+    acce_uncali = np.array(acce_uncali)
+    gyro = np.array(gyro)
+    gyro_uncali = np.array(gyro_uncali)
+    magn = np.array(magn)
+    magn_uncali = np.array(magn_uncali)
+    ahrs = np.array(ahrs)
+    wifi = np.array(wifi)
+    ibeacon = np.array(ibeacon)
+    waypoint = np.array(waypoint)
+    
+    return ReadData(acce, acce_uncali, gyro, gyro_uncali, magn, magn_uncali, ahrs, wifi, ibeacon, waypoint)
+  
+    print('Workspace not found')
+   
+# Get a named datastore from the current workspace
+datastore = Datastore.get(ws, datastore_name='linraw')
+
+datastore.download("./ilnraw")
+
+import os
+os.chdir("./ilnraw")
+
+import glob
+list_files = glob.glob("./raw/train/**/*.txt", recursive = True)
+list_files[:20]
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+from dataclasses import dataclass
+
+import matplotlib.pyplot as plt # visualization
+plt.rcParams.update({'font.size': 14})
+import seaborn as sns # visualization
+
+import warnings # Supress warnings 
+warnings.filterwarnings('ignore')
+
+from tqdm import tqdm
+
+import json
+import plotly.graph_objs as go
+from PIL import Image
+
+@dataclass
+class ReadData:
+    acce: np.ndarray
+    acce_uncali: np.ndarray
+    gyro: np.ndarray
+    gyro_uncali: np.ndarray
+    magn: np.ndarray
+    magn_uncali: np.ndarray
+    ahrs: np.ndarray
+    wifi: np.ndarray
+    ibeacon: np.ndarray
+    waypoint: np.ndarray
+
+
+def read_data_file(data_filename):
+    acce = []
+    acce_uncali = []
+    gyro = []
+    gyro_uncali = []
+    magn = []
+    magn_uncali = []
+    ahrs = []
+    wifi = []
+    ibeacon = []
+    waypoint = []
+
+    with open(data_filename, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+    for line_data in lines:
+        line_data = line_data.strip()
+        if not line_data or line_data[0] == '#':
+            continue
+
+        line_data = line_data.split('\t')
+
+        if line_data[1] == 'TYPE_WAYPOINT':
+            waypoint.append([int(line_data[0]), float(line_data[2]), float(line_data[3])])
+            continue
+       
+        if line_data[1] == 'TYPE_ACCELEROMETER':
+            acce.append([int(line_data[0]), float(line_data[2]), float(line_data[3]), float(line_data[4])])
+            continue
+        
+        if line_data[1] == 'TYPE_ACCELEROMETER_UNCALIBRATED':
+            acce_uncali.append([int(line_data[0]), float(line_data[2]), float(line_data[3]), float(line_data[4])])
+            continue
+        
+        if line_data[1] == 'TYPE_GYROSCOPE':
+            gyro.append([int(line_data[0]), float(line_data[2]), float(line_data[3]), float(line_data[4])])
+            continue
+
+        if line_data[1] == 'TYPE_GYROSCOPE_UNCALIBRATED':
+            gyro_uncali.append([int(line_data[0]), float(line_data[2]), float(line_data[3]), float(line_data[4])])
+            continue
+        
+        if line_data[1] == 'TYPE_MAGNETIC_FIELD':
+            magn.append([int(line_data[0]), float(line_data[2]), float(line_data[3]), float(line_data[4])])
+            continue
+
+        if line_data[1] == 'TYPE_MAGNETIC_FIELD_UNCALIBRATED':
+            magn_uncali.append([int(line_data[0]), float(line_data[2]), float(line_data[3]), float(line_data[4])])
+            continue
+
+        if line_data[1] == 'TYPE_ROTATION_VECTOR':
+            ahrs.append([int(line_data[0]), float(line_data[2]), float(line_data[3]), float(line_data[4])])
+            continue
+
+        if line_data[1] == 'TYPE_WIFI':
+            sys_ts = line_data[0]
+            ssid = line_data[2]
+            bssid = line_data[3]
+            rssi = line_data[4]
+            lastseen_ts = line_data[6]
+            wifi_data = [sys_ts, ssid, bssid, rssi, lastseen_ts]
+            wifi.append(wifi_data)
+            continue
+
+        if line_data[1] == 'TYPE_BEACON':
+            ts = line_data[0]
+            uuid = line_data[2]
+            major = line_data[3]
+            minor = line_data[4]
+            rssi = line_data[6]
+            ibeacon_data = [ts, '_'.join([uuid, major, minor]), rssi]
+            ibeacon.append(ibeacon_data)
+            continue
+        
+    
+    acce = np.array(acce)
+    acce_uncali = np.array(acce_uncali)
+    gyro = np.array(gyro)
+    gyro_uncali = np.array(gyro_uncali)
+    magn = np.array(magn)
+    magn_uncali = np.array(magn_uncali)
+    ahrs = np.array(ahrs)
+    wifi = np.array(wifi)
+    ibeacon = np.array(ibeacon)
+    waypoint = np.array(waypoint)
+    
+    return ReadData(acce, acce_uncali, gyro, gyro_uncali, magn, magn_uncali, ahrs, wifi, ibeacon, waypoint)
+def visualize_trajectory(trajectory, floor_plan_filename, width_meter, height_meter, title=None, mode='lines + markers + text', show=False):
+    """
+    Copied from from https://github.com/location-competition/indoor-location-competition-20/blob/master/visualize_f.py
+
+    """
+    fig = go.Figure()
+
+    # add trajectory
+    size_list = [6] * trajectory.shape[0]
+    size_list[0] = 10
+    size_list[-1] = 10
+
+    color_list = ['rgba(4, 174, 4, 0.5)'] * trajectory.shape[0]
+    color_list[0] = 'rgba(12, 5, 235, 1)'
+    color_list[-1] = 'rgba(235, 5, 5, 1)'
+
+    position_count = {}
+    text_list = []
+    for i in range(trajectory.shape[0]):
+        if str(trajectory[i]) in position_count:
+            position_count[str(trajectory[i])] += 1
+        else:
+            position_count[str(trajectory[i])] = 0
+        text_list.append('        ' * position_count[str(trajectory[i])] + f'{i}')
+    text_list[0] = 'Start 0'
+    text_list[-1] = f'End {trajectory.shape[0] - 1}'
+
+    fig.add_trace(
+        go.Scattergl(
+            x=trajectory[:, 0],
+            y=trajectory[:, 1],
+            mode=mode,
+            marker=dict(size=size_list, color=color_list),
+            line=dict(shape='linear', color='lightgrey', width=3, dash='dash'),
+            text=text_list,
+            textposition="top center",
+            name='trajectory',
+        ))
+
+    # add floor plan
+    floor_plan = Image.open(floor_plan_filename)
+    fig.update_layout(images=[
+        go.layout.Image(
+            source=floor_plan,
+            xref="x",
+            yref="y",
+            x=0,
+            y=height_meter,
+            sizex=width_meter,
+            sizey=height_meter,
+            sizing="contain",
+            opacity=1,
+            layer="below",
+        )
+    ])
+
+    # configure
+    fig.update_xaxes(autorange=False, range=[0, width_meter])
+    fig.update_yaxes(autorange=False, range=[0, height_meter], scaleanchor="x", scaleratio=1)
+    fig.update_layout(
+        title=go.layout.Title(
+            text=title or "No title.",
+            xref="paper",
+            x=0,
+        ),
+        autosize=True,
+        width=800,
+        height=  800 * height_meter / width_meter,
+        template="plotly_white",
+    )
+
+    if show:
+        fig.show()
+
+    return fig
+
+def visualize_train_trajectory(path):
+    """
+    Edited from 
+    https://www.kaggle.com/ihelon/indoor-location-exploratory-data-analysis
+    """
+    _id, floor = path.split("/")[3:5]
+    
+    train_floor_data = read_data_file(path)
+    with open(f"raw/metadata/{_id}/{floor}/floor_info.json") as f:
+        train_floor_info = json.load(f)
+
+    return visualize_trajectory(
+        train_floor_data.waypoint[:, 1:3], 
+        f"raw/metadata/{_id}/{floor}/floor_image.png",
+        train_floor_info["map_info"]["width"], 
+        train_floor_info["map_info"]["height"],
+        f"Visualization of {path}"
+    )
+    
+filename = list_files[0]
+filename
+
+visualize_train_trajectory(filename)
